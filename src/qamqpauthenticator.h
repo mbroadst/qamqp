c4524b5204638f27d7028610c0631edf404f1227
Matt Broadstone <mbroadstone@devonit.com>
2015-02-09 13:45:05 -0500

    update LICENSE and add license headers

--- src/qamqpauthenticator.h
+++ src/qamqpauthenticator.h
@@ -1,3 +1,20 @@
+/*
+ * Copyright (C) 2012-2014 Alexey Shcherbakov
+ * Copyright (C) 2014-2015 Matt Broadstone
+ * Contact: https://github.com/mbroadst/qamqp
+ *
+ * This file is part of the QAMQP Library.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ */
 #ifndef QAMQPAUTHENTICATOR_H
 #define QAMQPAUTHENTICATOR_H
 
