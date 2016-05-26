db12b3549078a1835114d449921684f4c8f654c1
Matt Broadstone <mbroadst@gmail.com>
2016-05-26 08:26:55 -0400

    fix(build): qt5 for 12.04 has moved PPAs

--- tests/files/travis/test-deps.sh
+++ tests/files/travis/test-deps.sh
@@ -4,7 +4,7 @@ if [ "$QT_SELECT" = "qt4" ]; then
     sudo apt-get update
     sudo apt-get install libqt4-dev
 else
-    sudo add-apt-repository -y ppa:ubuntu-sdk-team/ppa
+    sudo add-apt-repository -y ppa:canonical-qt5-edgers/ubuntu1204-qt5
     sudo apt-get update
     sudo apt-get install qtbase5-dev
 fi
